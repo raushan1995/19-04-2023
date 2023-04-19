faaf2482e9884d17c9bfceafefac5ed53c620e4c
Raushan Kumar <priyanshu1996r@gmail.com>
2023-04-19 16:58:08 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularForms';
+  defaultCountry = 'india';
+
+  firstname:string;
+  lastname:string;
+  email: string;
+ 
+
+ 
+
+  
+  @ViewChild('myForm') form: NgForm;
+
+  onSubmit(){
+    console.log(this.form);
+
+    this.firstname = this.form.value.personDetails.firstname;
+    this.lastname = this.form.value.personDetails.lastname;
+    this.email = this.form.value.personDetails.email;
+    
+
+    this.form.reset();
+  }
+
+  setDefaultValues(){
+    // this.form.value.personDetails.firstname = 'John';
+    // this.form.value.personDetails.lastname = 'smith';
+    // this.form.value.personDetails.email = 'abc@example.com';
+    // this.form.setValue({
+    //  
+    //   personDetails: {
+    //     firstname: 'John',
+    //     lastname: 'Smith',
+    //     email: 'abc@example.com'
+    //   }
+    // })
+
+    this.form.form.patchValue({
+      personDetails: {
+         firstname: 'John',
+         lastname: 'Smith',
+         email: 'abc@example.com'
+      }
+    })
+  }
+}
